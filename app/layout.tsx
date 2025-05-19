576112f3d1609c70eb80ce347fbb60fc51dc350e
nihadot <mysonisfunny@gmail.com>
2025-05-19 12:51:34 +0530

    google tag manager code added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,9 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ReduxProvider } from "@/redux/provider";
-import { Toaster } from 'react-hot-toast'
+import { Toaster } from "react-hot-toast";
 import AppInitializer from "./AppInitializer";
-import OfflineFallback from "@/components/OfflineFallback";
-import InternetCheck from "@/components/InternetCheck";
+import Script from "next/script";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -23,17 +22,33 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-
   return (
     <html lang="en" className="">
+      <head>
+        <Script
+          id="gtm-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PB5XRT9G');
+            `,
+          }}
+        />
+      </head>
       <body className={`${poppins.variable} antialiased`}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PB5XRT9G"
+          height="0" width="0"
+          style={{ display: "none", visibility: "hidden" }}
+
+        ></iframe></noscript>
         <ReduxProvider>
-          <InternetCheck>
-            <AppInitializer />
-            <Toaster />
-            {children}
-          </InternetCheck>
+          <AppInitializer />
+          <Toaster />
+          {children}
         </ReduxProvider>
       </body>
     </html>
